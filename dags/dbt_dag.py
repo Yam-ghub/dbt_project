acfbec193fcadb563f77fb98d6b8d70b474253c2
unknown <ramos.williamlaurence@gmail.com>
2026-08-18 17:49:45 +0800

    Integrating orchaestration using airflow

--- dags/dbt_dag.py
+++ dags/dbt_dag.py
@@ -1,9 +1,11 @@
 import os
+from pathlib import Path
 from datetime import datetime
 
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
+DBT_PROJECT_DIR = Path(__file__).parent / "dbt" / "data_pipeline"
 
 profile_config = ProfileConfig(
     profile_name="default",
@@ -15,12 +17,12 @@ profile_config = ProfileConfig(
 )
 
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig("/Users/William/AppData/Roaming/Python313/Scripts/dbt_dag/dbt/data_pipeline",),
+    project_config=ProjectConfig(str(DBT_PROJECT_DIR)),
     operator_args={"install_deps": True},
     profile_config=profile_config,
     execution_config=ExecutionConfig(dbt_executable_path=f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt",),
-    schedule_interval="@daily",
-    start_date=datetime(2026, 18, 8),
+    schedule="@daily",
+    start_date=datetime(2026, 8, 18),
     catchup=False,
     dag_id="dbt_dag",
 )
